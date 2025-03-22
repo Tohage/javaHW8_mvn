f3ffb624d3c75ca383b8a13e2c8f198c868b98d0
Tohage89 <avgorin89@gmail.com>
2025-03-22 21:18:44 +0300

    add class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Post post = new Post();
+        post.name = "Иван";
+        post.surname = "Иванов";
+        post.patronymic = "Иванович";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+
+        post.passport = "4444 № 44444444";
+        post.phone = "+7 (999)-999-99-99";
+        post.subscription = true;
+
     }
 }
---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+
+    public String phone;
+    public boolean subscription;
+    public String passport;
+
+    public FormDate birthday;
+
+}
